a250a5e5ac832c211a452d25d0a70d35b8caa9d5
Jean Park <jyp033@ucsd.edu>
2016-04-13 21:42:04 -0700

    name and date with message

--- public/js/chatbox.js
+++ public/js/chatbox.js
@@ -17,19 +17,27 @@
     // other possible solution(s) here.
     $('#messages').prepend($('<li>').html(messageTemplate(parsedData)));
 
-    function messageTemplate(parsedData) {
-      // generate HTML text based on some data to be prepended into the list
-    }
 });
     // You may use this for updating new message
     function messageTemplate(template) {
+        console.log(template.user);
+        var date_obj = new Date(template.posted)
+        var options = {
+              year: "numeric",
+              month: "numeric",
+              day: "numeric"
+            };
+            
+        var display_date = date_obj.toLocaleDateString('en-US', options);
+
+
         var result = '<div class="user">' +
             '<div class="user-image">' +
             '<img src="' + template.user.photo + '" alt="">' +
             '</div>' +
             '<div class="user-info">' +
-            '<span class="username">' + template.user.username + '</span><br/>' +
-            '<span class="posted">' + template.posted + '</span>' +
+            '<span class="username">' + template.user + '</span><br/>' +
+            '<span class="posted">' + display_date + '</span>' +
             '</div>' +
             '</div>' +
             '<div class="message-content">' +
